d3ff453872b4824b7251bd5fd1680cb77503141d
ɓ૨σҡεɳ3 <124243079+spivan19@users.noreply.github.com>
2025-05-26 10:27:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
+  packname: 'Kami sama',
+  author: 'ＫɅ爪工‎',
+  botName: "ＫɅ爪工-Md",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '33756806397', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  commandMode: "private",
+  description: "By Kami for Kami.",
   version: "2.0.1",
 };
 
